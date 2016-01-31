3a97b93300a7a426468b257341d2e0e800f742a9
Fabio Battarra <fabio.battarra@gmail.com>
2016-01-31 21:42:08 +0100

    - "update in progress" image while copying downloaded content to live folder - GITHub URL in webroom.txt - "update in progress" image

--- src/home/pi/mirror.sh
+++ src/home/pi/mirror.sh
@@ -12,6 +12,8 @@ LOGFTP=$LOGFTP
 LOGRSYNC="/home/pi/logs/rsync/$(date +'%Y-%m-%d').log"
 LOGRSYNC=$LOGRSYNC
 
+UPDATEIMG="/home/pi/updating.png"
+
 printf "\n###############################\n" >> $LOGFTP
 printf "# script launched at $(date +'%T') #\n" >> $LOGFTP
 printf "###############################\n" >> $LOGFTP
@@ -24,19 +26,38 @@ user $USER $PASS
 set ssl:verify-certificate no
 mirror --continue --parallel=10 --only-newer --delete --verbose=2 $SOURCEFOLDER $TARGETFOLDER
 bye
-" >> $LOGFTP && printf "\ndone.\n\n" >> $LOGFTP 
+" >> $LOGFTP
+printf "\ndone.\n\n" >> $LOGFTP 
 
 printf "\n###############################\n" >> $LOGRSYNC
 printf "# script launched at $(date +'%T') #\n" >> $LOGRSYNC
 printf "###############################\n" >> $LOGRSYNC
 printf "\n" >> $LOGRSYNC
 
-printf "stopping looper ..." >> $LOGRSYNC
-sudo /etc/init.d/videoloop stop >> $LOGRSYNC && printf " ...done\n\n" >> $LOGRSYNC
+printf "stopping looper ...\n" >> $LOGRSYNC
+sudo service videoloop stop >> $LOGRSYNC
+printf " ...done\n\n" >> $LOGRSYNC
+
+printf "removing logo overlay ..." >> $LOGRSYNC
+sudo killall pngview >> $LOGRSYNC
+printf " ...done\n\n" >> $LOGRSYNC
+
+printf "removing logo overlay ..." >> $LOGRSYNC
+sudo killall pngview >> $LOGRSYNC
+printf " ...done\n\n" >> $LOGRSYNC
+
+printf "showing 'update in progress' image ..." >> $LOGRSYNC
+/home/pi/raspidmx-master/pngview/pngview -l 1 -x 0 -y 0 $UPDATEIMG &
+printf " ...done\n\n" >> $LOGRSYNC
 
 printf "synchronizing tmp-videos with videos folder:\n" >> $LOGRSYNC
-rsync -avzh --delete /home/pi/tmp-videos/ /home/pi/videos >> $LOGRSYNC && printf "\ndone.\n\n" >> $LOGRSYNC
+rsync -avzh --delete /home/pi/tmp-videos/ /home/pi/videos >> $LOGRSYNC
+printf "\ndone.\n\n" >> $LOGRSYNC
+
+printf "removing 'update in progress' image ..." >> $LOGRSYNC
+sudo killall pngview >> $LOGRSYNC
+printf " ...done\n\n" >> $LOGRSYNC
 
 printf "\nstarting looper..." >> $LOGRSYNC
-sudo /etc/init.d/videoloop start >> $LOGRSYNC && printf " ...done\n\n" >> $LOGRSYNC
-
+sudo service videoloop start >> $LOGRSYNC &
+printf " ...done\n\n" >> $LOGRSYNC
